e9371ac23d488da0c55b2315d7a9995022cb64c2
HossainIdrish <139071336+HossainIdrish@users.noreply.github.com>
2024-01-24 11:32:37 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,40 @@
-var mymap = L.map('mapid').setView([45.5, -120], 10);
-//https://a.tile.openstreetmap.org/{z}/{x}/{y}.png
-//'http://a.tile.stamen.com/toner/{z}/{x}/{y}.png'
-//https://wiki.openstreetmap.org/wiki/Tile_servers
-L.tileLayer('https://a.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: 'Map tiles by Stamen Design, under CC BY 3.0.',
-        maxZoom: 18
-}).addTo(mymap);
+// Function to create a custom icon
+function createCustomIcon(iconUrl) {
+    return L.icon({
+        iconUrl: iconUrl,
+        iconSize: [100, 70], 
+        iconAnchor: [16, 32]
+    });
+}
 
-    var marker1 = L.marker([38.6247, -90.1848]).addTo(mymap);
-    var marker2 = L.marker([38.6128, -90.2591]).addTo(mymap);
-    var marker3 = L.marker([38.6383, -90.2928]).addTo(mymap);
-    var marker4 = L.marker([38.6403, -90.2849]).addTo(mymap);
-    var marker5 = L.marker([38.6339, -90.2004]).addTo(mymap);
-   
-marker1.bindPopup("<b>Gateway Arch</b><br>The iconic 630-ft-tall monument.").openPopup();
-marker2.bindPopup("<b>Missouri Botanical Garden</b><br>79 acres of horticultural display.").openPopup();
-marker3.bindPopup("<b>St. Louis Zoo</b><br>A leading zoo with free admission.").openPopup();
-marker4.bindPopup("<b>Forest Park</b><br>One of the largest urban parks in the USA.").openPopup();
-marker5.bindPopup("<b>City Museum</b><br>A museum featuring repurposed architectural and industrial objects.").openPopup();
+// Initialize the map
+var map = L.map('map').setView([38.6270, -90.1994], 13);
+
+// Add OpenStreetMap tiles to the map
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '© OpenStreetMap contributors'
+}).addTo(map);
+
+// Create custom icons using the function
+var customIcon_m1 = createCustomIcon('https://afar.brightspotcdn.com/dims4/default/5d8cd0c/2147483647/strip/false/crop/1900x1267+0+0/resize/1486x991!/quality/90/?url=https%3A%2F%2Fafar-media-production-web.s3.us-west-2.amazonaws.com%2Fbrightspot%2F56%2F58%2F95a33bbf1bb47d535be0c0bb270f%2Foriginal-c07a2d169f29f58b9af7666d94661776.jpg');
+var customIcon_m2 = createCustomIcon('https://www.peachythemagazine.com/wp-content/uploads/2021/09/forest-park-st-louis01.jpg');
+var customIcon_m3 = createCustomIcon('https://www.slu.edu/globalcitizenship/imgs/center-global-citizenship.jpg');
+var customIcon_m4 = createCustomIcon('https://www.missouribotanicalgarden.org/portals/0/2022redesignassets/MBG/garden-hours-img.jpg?ver=yGx16y502fdmmQBGcQShZw%3D%3D');
+var customIcon_m5 = createCustomIcon('https://images.squarespace-cdn.com/content/v1/5ec906611a267019b81168db/53b02d0a-a78f-4d1c-be5e-a1b7c79d514b/278197950_5264774750212588_203632726040177783_n.jpg');
+
+// Adding individual markers
+var marker1 = L.marker([38.6247, -90.1848], { icon: customIcon_m1 }).addTo(map);
+marker1.bindPopup("<b>The Gateway Arch!</b><br>A 630-foot-tall monument in St. Louis.");
+
+var marker2 = L.marker([38.6383, -90.2876], { icon: customIcon_m2 }).addTo(map);
+marker2.bindPopup("<b>Forest Park</b><br>One of the most beautiful urban parks in the world!!");
+
+var marker3 = L.marker([38.6360, -90.2367], { icon: customIcon_m3 }).addTo(map);
+marker3.bindPopup("<b>Des Peres Hall, SLU</b><br>My Workspace: Remote Sensing Lab");
+
+var marker4 = L.marker([38.6128, -90.2590], { icon: customIcon_m4 }).addTo(map);
+marker4.bindPopup("<b>Missouri Botanical Garden</b><br>A beautiful botanical garden!");
+
+var marker5 = L.marker([38.6450, -90.2612], { icon: customIcon_m5 }).addTo(map);
+marker5.bindPopup("<b>Clementine's Naughty & Nice Creamery</b><br>Best Ice-Cream in St. Louis!");
